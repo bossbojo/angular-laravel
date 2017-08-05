18fd4e62354bbd6295cd87545269594d3d717489
bossbojo44 <paramat@addlink.com>
2017-08-05 17:53:52 +0700

    asdasd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  User:Users[]=[];
   constructor() { }
 
   ngOnInit() {
   }
+  
+  onAdd(name:string,email:string,detail:string){
+    console.log(name+' '+email+' '+detail);
+    
+    this.User.push(new Users(name,email,detail));
+  }
 
+}
+
+export class Users {
+  constructor(name: string, email: string , detail:string) {
+    this.name = name;
+    this.email = email;
+    this.detail = detail;
+  }
+  name: String;
+  email: String;
+  detail: String;
 }
