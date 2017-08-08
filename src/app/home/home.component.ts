53998ed1ff03c970d3b6b665fae2c6bfd536d7ae
BiG-BosS <paramat.si.56@ubu.ac.th>
2017-08-08 23:11:57 +0700

    update detial modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+declare let $;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  User:Users[]=[];
-  constructor() { }
+  User: Users[] = [];
+  constructor() {
+    this.User.push(new Users('Paramat Singkon','paramat@ubu.ac.th','just code that so easy.'));
+  }
 
   ngOnInit() {
   }
-  
-  onAdd(name:string,email:string,detail:string){
-    console.log(name+' '+email+' '+detail);
-    
-    this.User.push(new Users(name,email,detail));
+
+  onAdd(name: string, email: string, detail: string) {
+    console.log(name + ' ' + email + ' ' + detail);
+
+    this.User.push(new Users(name, email, detail));
+  }
+  deleteItem(item) {
+    this.User.splice(item, 1);
+  }
+  onDetail(item){
+    $('#userid').html(item);
+    $('#userName').html(this.User[item].name);
+    $('#userEmail').html(this.User[item].email);
+    $('#userDetail').html(this.User[item].detail);
   }
 
 }
 
 export class Users {
-  constructor(name: string, email: string , detail:string) {
+  constructor(name: string, email: string, detail: string) {
     this.name = name;
     this.email = email;
     this.detail = detail;
